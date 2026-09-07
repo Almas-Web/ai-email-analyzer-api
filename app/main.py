7b3f757b0d1e3f56d592bc5af114ed020268c656
Almas Hossen <almashossen028@gmail.com>
2026-09-07 23:00:44 +0600

    Fix CI database configuration

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
 from app.core.config import settings
-from app.core.database import engine, Base
 from app.routers import auth, emails, dashboard
-
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
